d5da86bdd41704b9c95ef6e613257ea7f4cc11d2
SOORAJ-006 <lsoorajlal5git@gmail.com>
2024-07-26 17:18:20 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const connectDB = require('./config/connectDB')
+const dotenv = require('dotenv').config()
+const cors = require('cors')
+const app = express()
+
+
+const port = process.env.PORT || 3000
+connectDB()
+
+app.use(cors());
+app.use(express.json())
+
+app.use('/tasks' , require('./routes/task.routes'))
+app.use('/users', require('./routes/users.routes'))
+
+app.listen(port , () => {
+    console.log(`server running on ${port}`)
+})
